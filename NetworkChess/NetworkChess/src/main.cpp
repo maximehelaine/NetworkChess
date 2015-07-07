19e65f1d5e04081cff627712d75c1991f754f4c5
ArthurTorrente <torrente.arthur@gmail.com>
2015-07-07 16:46:47 +0200

    Mise en place de FMOD

--- NetworkChess/NetworkChess/src/main.cpp
+++ NetworkChess/NetworkChess/src/main.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "SDL.h"
 
+#include "fmod.h"
+
 int main(int argc, char** argv)
 {
     if (SDL_Init(SDL_INIT_VIDEO) == -1)
@@ -9,7 +11,13 @@ int main(int argc, char** argv)
 
     }
 
+    FMOD_SYSTEM* sys;
 
+    FMOD_System_Create(&sys);
+    FMOD_System_Init(sys, 2, FMOD_INIT_NORMAL, nullptr);
+
+    FMOD_System_Close(sys);
+    FMOD_System_Release(sys);
 
     SDL_Quit();
 
--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
 rem take path to Cinder
 
 setx Lib_SDL ""
+setx Lib_FMOD "C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\lowlevel"
